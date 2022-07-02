aa0c3908f9e8b2726fee53a2810f1f142590ae69
AbigailSc <abigailsarzuri@gmail.com>
2022-07-02 05:19:59 -0300

    Base de datos terminada

--- PI-Food-main/api/src/models/Recipe.js
+++ PI-Food-main/api/src/models/Recipe.js
@@ -1,12 +1,52 @@
-const { DataTypes } = require('sequelize');
+const { DataTypes, UUIDV4 } = require('sequelize');
 // Exportamos una funcion que define el modelo
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define('recipe', {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue:UUIDV4,
+      primaryKey: true,
+      allowNull: false
+    },
     name: {
       type: DataTypes.STRING,
       allowNull: false,
+      unique: true,
+      validate: {
+        isAlpha: true
+      }
     },
+    summary: {
+      type: DataTypes.STRING(1000),
+      allowNull: false
+    },
+    score: {
+      type: DataTypes.FLOAT(1),
+      validate: {
+        min: 0,
+        max: 100
+      }
+    },
+    healthScore: {
+      type: DataTypes.FLOAT(1),
+      validate: {
+        min: 0,
+        max: 100
+      }
+    },
+    steps: {
+      type: DataTypes.STRING
+    },
+    image: {
+      type: DataTypes.STRING,
+      defaultValue: "https://data.whicdn.com/images/338252817/original.jpg"
+    },
+    createdInDb: {
+      type: Datatypes.BOOLEAN,
+      allowNull: false,
+      defaultValue: true
+    }
   });
 };
