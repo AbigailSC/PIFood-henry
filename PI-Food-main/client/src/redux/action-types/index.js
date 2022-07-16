04bb3fa717e10df2ee86ff45d09c2f73cc26a674
AbigailSc <abigailsarzuri@gmail.com>
2022-07-16 10:32:24 -0300

    Redux completo

---
+++ PI-Food-main/client/src/redux/action-types/index.js
@@ -0,0 +1,9 @@
+export const GET_DIETS = 'GET_DIETS';
+export const FILTER_NAME = 'FILTER_NAME';
+export const CLEAN_RECIPE = 'CLEAN_RECIPE';
+export const SEARCH_NAME = 'SEARCH_NAME';
+export const FILTER_DIETS = 'FILTER_DIETS';
+export const GET_RECIPES = 'GET_RECIPES';
+export const GET_RECIPE_BY_ID = 'GET_RECIPE_BY_ID'
+export const FILTER_SCORE = 'FILTER_SCORE'
+export const POST_RECIPE = 'POST_RECIPE'
---
+++ PI-Food-main/client/src/redux/reducer/index.js
@@ -0,0 +1,94 @@
+import {
+    GET_DIETS,
+    FILTER_NAME,
+    CLEAN_RECIPE,
+    SEARCH_NAME,
+    FILTER_DIETS,
+    FILTER_SCORE,
+    GET_RECIPES,
+    GET_RECIPE_BY_ID,
+    POST_RECIPE
+} from '../action-types/index.js';
+
+const initialState = {
+    recipes: [],
+    copyRecipes: [],
+    detail: [],
+    diets: [],
+}
+
+export function rootReducer(state = initialState, action){
+    switch(action.type){
+        case GET_DIETS:
+            return{
+                ...state,
+                diets: action.payload
+            }
+        case POST_RECIPE:
+            return{
+                ...state
+            }
+        case CLEAN_RECIPE:
+            return{
+                ...state,
+                detail:[]
+            }
+        case SEARCH_NAME:
+            return{
+                ...state,
+                recipes: action.payload
+            }
+        case FILTER_DIETS:
+            const copyRecipes = state.copyRecipes;
+            //console.log(copyRecipes, "fideos con tuco")
+            const dietsFilter = action.payload === 'all' ? copyRecipes : 
+            copyRecipes.filter((recipe) => {
+                let diet = recipe.diets.map((dietName) => {
+                    if(dietName.name){
+                        return dietName.name
+                    }
+                    return dietName
+                })
+                if(diet.includes(action.payload)){
+                    return recipe
+                }
+            })
+            //console.log("Milanga", dietsFilter)
+            return{
+                ...state,
+                recipes: dietsFilter
+            }
+        case FILTER_NAME:
+            let orderName = action.payload === 'asc' ? 
+            state.recipes.sort((a, b) => a.name.toLowerCase() > b.name.toLowerCase() ? 1 : -1)
+            :
+            state.recipes.sort((a, b) => a.name.toLowerCase() < b.name.toLowerCase() ? 1 : -1)
+            return{
+                ...state,
+                recipes: orderName
+            }
+        case FILTER_SCORE:
+            let orderScore = action.payload === 'high' ? 
+            state.recipes.sort((a, b) => a.healthScore < b.healthScore ? 1 : -1) 
+            : 
+            state.recipes.sort((a, b) => a.healthScore < b.healthScore ? -1 : 1)
+            return{
+                ...state,
+                recipes: orderScore
+            }
+        case GET_RECIPES:
+            return{
+                ...state,
+                recipes: action.payload,
+                copyRecipes: action.payload,
+                detail: []
+            }
+        case GET_RECIPE_BY_ID:
+            return{
+                ...state,
+                detail: action.payload
+            }
+        default:
+            return state
+    }
+}
